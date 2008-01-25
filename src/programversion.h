3fbc5a83a2646cbd9ad0eacfe4e93b07ba3fca01
Xesc <xeskuu.xvst@gmail.com>
2008-01-25 15:01:37 +0000

    Changed program version to: "1.4 Alpha"

--- src/programversion.h
+++ src/programversion.h
@@ -28,6 +28,6 @@
 #ifndef __PROGRAMVERSION_H__
 #define __PROGRAMVERSION_H__
 
-static const QString PROGRAM_VERSION = "1.3 Alpha"; //<! Current program version
+static const QString PROGRAM_VERSION = "1.4 Alpha"; //<! Current program version
 
 #endif // __PROGRAMVERSION_H__
