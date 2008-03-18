4f03830814774b07af603a53bc7adef07e086b42
Xesc <xeskuu.xvst@gmail.com>
2008-03-18 04:01:27 +0000

    - Changed licence to GNU GPL v3 (http://www.gnu.org/licenses/gpl.html)

--- src/forms/infoviewimpl.h
+++ src/forms/infoviewimpl.h
@@ -3,28 +3,25 @@
 * This file is part of xVideoServiceThief, 
 * an open-source cross-platform Video service download
 *
-* Copyright (C) 2007 Xesc & Technology
+* Copyright (C) 2007 - 2008 Xesc & Technology
 *
-* This program is free software; you can redistribute it and/or modify
+* This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
+* the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-* GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+* along with xVideoServiceThief. If not, see <http://www.gnu.org/licenses/>.
 *
-* Contact e-mail: Xesc <xeskuu@gmail.com>
+* Contact e-mail: Xesc <xeskuu.xvst@gmail.com>
 * Program URL   : http://xviservicethief.sourceforge.net/
 *
 */
-
 
 #ifndef INFOVIEWIMPL_H
 #define INFOVIEWIMPL_H
