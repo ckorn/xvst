0538f8b4682f4f2b3bf776c7e774892a8e22198e
Xesc <xeskuu.xvst@gmail.com>
2009-05-11 10:13:20 +0000

    - Changed program version to 2.0.2

--- src/programversion.h
+++ src/programversion.h
@@ -26,8 +26,8 @@
 #ifndef __PROGRAMVERSION_H__
 #define __PROGRAMVERSION_H__
 
-static const QString PROGRAM_VERSION		= "2.0.1 Alpha"; 	//<! Current program version
-static const QString PROGRAM_VERSION_SHORT	= "2.0.1a"; 		//<! Current program version (short version)
+static const QString PROGRAM_VERSION		= "2.0.2 Alpha"; 	//<! Current program version
+static const QString PROGRAM_VERSION_SHORT	= "2.0.2a"; 		//<! Current program version (short version)
 
 #ifdef Q_OS_WIN32
 static const QString CURRENT_OS = "MSWINDOWS"; //<! Microsoft windows version
