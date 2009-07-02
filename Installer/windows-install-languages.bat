bbd110e017035d601a73e3b9f485c17f1cda5cf4
Xesc <xeskuu.xvst@gmail.com>
2009-07-02 13:15:24 +0000

    - Fixed bug with windows-install-languages.bat and windows-install-plugins.bat

--- Installer/windows-install-languages.bat
+++ Installer/windows-install-languages.bat
@@ -1,3 +1,4 @@
+set oldDir=%CD%
 if not "%1" == "" cd "%1"
 rmdir "..\bin\languages" /s /q
 mkdir "..\bin\languages"
@@ -5,3 +6,4 @@ FOR /R "..\resources\translations" %%v IN (*.qm) DO copy "%%v" "..\bin\languages
 FOR /R "..\resources\translations" %%v IN (*.language) DO copy "%%v" "..\bin\languages"
 del "..\bin\languages\template_for_new_translations.qm"
 del "..\bin\languages\xVST_it.qm"
+cd %oldDir%
