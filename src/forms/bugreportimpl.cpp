238e4ff6f9df910ab022c80c9cfd88b6b6a41860
Xesc <xeskuu.xvst@gmail.com>
2010-01-31 08:32:43 +0000

    - Added version 2.3.3 to bugreport

--- src/forms/bugreportimpl.cpp
+++ src/forms/bugreportimpl.cpp
@@ -74,6 +74,7 @@ BugReportImpl::BugReportImpl(ProgramOptions *programOptions, QWidget * parent, Q
 	trackerReport->addGroup("2.3",    "971205");
 	trackerReport->addGroup("2.3.1",  "1009922");
 	trackerReport->addGroup("2.3.2",  "1025880");
+	trackerReport->addGroup("2.3.3",  "1047982");
 	// signals
 	connect(spbViewInfo, SIGNAL(clicked()), this, SLOT(viewInfoClicked()));
 	connect(btnSend, SIGNAL(clicked()), this, SLOT(sendReportClicked()));
