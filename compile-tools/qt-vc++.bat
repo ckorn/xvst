c7653031e0d8f40fa190acf3b6762d192eeda9fd
Xesc <xeskuu.xvst@gmail.com>
2010-02-20 01:56:43 +0000

    - Added header for qt-vc++.bat

--- compile-tools/qt-vc++.bat
+++ compile-tools/qt-vc++.bat
@@ -1,6 +1,6 @@
-::echo xVideoServiceThief VC++ automated compiler script
-::echo Note: Configure with your envirotment variables before execute this Bat
-::echo Copyright Xesc and Technolgy 2010
+echo xVideoServiceThief VC++ automated compiler script
+echo Note: Configure with your envirotment variables before execute this Bat
+echo Copyright Xesc and Technolgy 2010
 
 set QtVersion=4.6.2
 set QtDir=C:\ProgramacioLibs\Qt\%QtVersion%-VC++
