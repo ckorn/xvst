1894ca219a7de3a122f345aca4eee40f57ba6104
Xesc <xeskuu.xvst@gmail.com>
2010-03-11 15:01:55 +0000

    - Added "winPenPack" in credits window

--- src/forms/creditsimpl.cpp
+++ src/forms/creditsimpl.cpp
@@ -30,8 +30,8 @@ CreditsImpl::CreditsImpl( QWidget * parent, Qt::WFlags f) : QDialog(parent, f)
 	setupUi(this);
 	// resize form if is needed
 #ifndef Q_OS_WIN32
-        resize(468, 590);
-        setMinimumSize(468, 590);
+		resize(468, 610);
+		setMinimumSize(468, 610);
 #endif
 }
 //
