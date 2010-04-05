dc451fb277eaf8b4dc2e4c4374b11c68af3e904d
Xesc <xeskuu.xvst@gmail.com>
2010-04-05 19:16:55 +0000

    - Added the russian translation

--- src/forms/creditsimpl.cpp
+++ src/forms/creditsimpl.cpp
@@ -30,8 +30,8 @@ CreditsImpl::CreditsImpl( QWidget * parent, Qt::WFlags f) : QDialog(parent, f)
 	setupUi(this);
 	// resize form if is needed
 #ifndef Q_OS_WIN32
-		resize(468, 610);
-		setMinimumSize(468, 610);
+		resize(468, 614);
+		setMinimumSize(468, 614);
 #endif
 }
 //
