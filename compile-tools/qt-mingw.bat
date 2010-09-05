ad39951fbd893c12963c04d8c374cb2874b599c5
Xesc <xeskuu.xvst@gmail.com>
2010-09-05 16:42:06 +0000

    - Changed Qt to 4.6.3

--- compile-tools/qt-mingw.bat
+++ compile-tools/qt-mingw.bat
@@ -2,7 +2,7 @@ echo xVideoServiceThief MinGW automated compiler script
 echo Note: Configure your envirotment variables before execute this Bat
 echo Copyright Xesc and Technolgy 2010
 
-set QtVersion=4.6.0
+set QtVersion=4.6.3
 set QtDir=C:\ProgramacioLibs\Qt\%QtVersion%\
 set UPXApp=C:\Documents and Settings\Administrador\Escritorio\upx.exe
 set MAKE=mingw32-make.exe
