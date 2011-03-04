3b3d262eb1b8504fc10aca95f02f183fe0fe9f6b
Xesc <xeskuu.xvst@gmail.com>
2011-03-04 15:34:45 +0000

    - Added finish and bulgarian automated language packager

--- Installer/create-language-pakages.sh
+++ Installer/create-language-pakages.sh
@@ -63,3 +63,9 @@ $PAKUNPAK_BIN -i "$LANGUAGES_DIR/updates/italiano_it_lang.update" "$LANGUAGES_DI
 
 # russian package
 $PAKUNPAK_BIN -i "$LANGUAGES_DIR/updates/russian_ru_lang.update" "$LANGUAGES_DIR/xVST_ru.qm" "$LANGUAGES_DIR/definitions/russian.language"
+
+# bulgarian package
+$PAKUNPAK_BIN -i "$LANGUAGES_DIR/updates/bulgarian_bg_lang.update" "$LANGUAGES_DIR/xVST_bg.qm" "$LANGUAGES_DIR/definitions/bulgarian.language"
+
+# finnish package
+$PAKUNPAK_BIN -i "$LANGUAGES_DIR/updates/finnish_fi_lang.update" "$LANGUAGES_DIR/xVST_fi.qm" "$LANGUAGES_DIR/definitions/finnish.language"
